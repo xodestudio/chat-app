94ca7dd5629a0ea9bd436db72676f7bd47841c6a
Ali Haider <alidevofficial@gmail.com>
2025-05-10 08:44:40 +0500

    Fix axios baseURL to remove double slashes in production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : `${import.meta.env.VITE_BACKEND_URL}/api`,
+      : `${import.meta.env.VITE_BACKEND_URL}/api`.replace(/([^:]\/)\/+/g, "$1"), // Removes double slashes
   withCredentials: true,
 });
